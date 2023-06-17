b21880fa3dc8265104dc8d380b9f29e349b4067a
guido <dev@guidoscialfa.com>
2023-06-17 17:10:53 +0200

    Liniting by @wordpress/scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-const path = require('path')
-const baseConfiguration = require('@wordpress/scripts/config/webpack.config')
-const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin')
+const path = require('path');
+const baseConfiguration = require('@wordpress/scripts/config/webpack.config');
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 module.exports = {
-  ...baseConfiguration,
-  plugins: [
-    ...baseConfiguration.plugins.filter(
-      plugin => plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
-    ),
-    new DependencyExtractionWebpackPlugin({
-      outputFormat: 'json',
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.tsx'],
-    alias: {
-      '@entities-search': path.resolve(__dirname, 'sources/js/src'),
-    },
-  },
-}
+	...baseConfiguration,
+	plugins: [
+		...baseConfiguration.plugins.filter(
+			(plugin) =>
+				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+		),
+		new DependencyExtractionWebpackPlugin({
+			outputFormat: 'json',
+		}),
+	],
+	resolve: {
+		extensions: ['.ts', '.tsx'],
+		alias: {
+			'@entities-search': path.resolve(__dirname, 'sources/js/src'),
+			'@entities-search-types': path.resolve(
+				__dirname,
+				'sources/js/src/@types'
+			),
+		},
+	},
+};
