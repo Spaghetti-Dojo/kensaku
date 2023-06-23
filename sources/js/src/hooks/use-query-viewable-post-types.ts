48b74c2487bdc1ce21da0902fb5c5c3af9870c22
guido scialfa <dev@guidoscialfa.com>
2023-06-24 00:14:42 +0200

    Add todo

--- sources/js/src/hooks/use-query-viewable-post-types.ts
+++ sources/js/src/hooks/use-query-viewable-post-types.ts
@@ -12,6 +12,7 @@ export function useQueryViewablePostTypes() {
 		(select: (store: string) => Selectors) => select('core'),
 		[]
 	);
+	// TODO Is it possible to `useEntityRecords`? So that we can send back resolving information?
 	const postTypes = coreSelectors.getPostTypes();
 
 	if (postTypes === null) {
