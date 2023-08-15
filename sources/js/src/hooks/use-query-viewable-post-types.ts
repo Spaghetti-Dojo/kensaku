5554863fce19fdc738f040d4521edb4bb34f428e
guido <dev@guidoscialfa.com>
2023-08-15 12:06:27 +0200

    Add documentation to `useQueryViewablePostTypes`

--- sources/js/src/hooks/use-query-viewable-post-types.ts
+++ sources/js/src/hooks/use-query-viewable-post-types.ts
@@ -3,6 +3,11 @@ import { Set } from 'immutable';
 
 import { useEntityRecords } from './use-entity-records';
 
+/**
+ * Hook to obtain the `viewable` post types only.
+ * This is an api on top of `useEntityRecords` to facilitate the usage of the `viewable` post types.
+ * @api
+ */
 export function useQueryViewablePostTypes(): EntitiesSearch.EntitiesRecords<EntitiesSearch.ViewablePostType> {
 	const entitiesRecords = useEntityRecords<EntitiesSearch.PostType<'edit'>>(
 		'root',
