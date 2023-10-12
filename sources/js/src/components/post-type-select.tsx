f57e830572296616b92b195077b16eacd0ff9d9f
guido <dev@guidoscialfa.com>
2023-10-12 22:43:33 +0200

    Hide internal implementation for `PostTypeSelect`
    Do not expose internal React Select properties to the outside of the component.

--- sources/js/src/components/post-type-select.tsx
+++ sources/js/src/components/post-type-select.tsx
@@ -1,28 +1,16 @@
 import EntitiesSearch from '@types';
 import React, { JSX } from 'react';
 import Select from 'react-select';
-import type { Props } from 'react-select';
 
-type SelectPropsWithoutOptions = Omit<
-	Props<
-		EntitiesSearch.ControlOption<EntitiesSearch.PostTypeSelect['value']>,
-		false
-	>,
-	keyof EntitiesSearch.PostTypeSelect | 'isMulti' | 'option'
->;
-
-interface PostTypeSelectProps
-	extends EntitiesSearch.PostTypeSelect,
-		SelectPropsWithoutOptions {}
-
-export function PostTypeSelect(props: PostTypeSelectProps): JSX.Element | null {
+export function PostTypeSelect(
+	props: EntitiesSearch.PostTypeSelect
+): JSX.Element | null {
 	const value = props.options.find((option) => option.value === props.value);
 
 	return (
 		<Select
-			{...props}
 			isMulti={false}
-			value={value ?? props.defaultValue ?? props.options.first() ?? null}
+			value={value ?? props.options.first() ?? null}
 			options={props.options.toArray()}
 			onChange={(opt) => props.onChange(opt?.value ?? null)}
 		/>
