ad9776ab1c2eaf4dbdf5af9d01e39f80c434404c
guido <dev@guidoscialfa.com>
2024-02-15 13:00:24 +0100

    Fix JS QA

--- tests/client/unit/vo/control-option.test.ts
+++ tests/client/unit/vo/control-option.test.ts
@@ -4,19 +4,19 @@ import { ControlOption } from '../../../../sources/client/src/vo/control-option'
 
 describe('ControlOption', () => {
 	it('should create a new ControlOption', () => {
-		const controlOption = ControlOption.new('label', 'value');
+		const controlOption = new ControlOption('label', 'value');
 		expect(controlOption.label).toBe('label');
 		expect(controlOption.value).toBe('value');
 	});
 
 	it('should throw an error when creating a new ControlOption with an empty label', () => {
-		expect(() => ControlOption.new('', 'value')).toThrow(
+		expect(() => new ControlOption('', 'value')).toThrow(
 			'ControlOption: Label must be a non empty string.'
 		);
 	});
 
 	it('should throw an error when creating a new ControlOption with an empty value', () => {
-		expect(() => ControlOption.new('label', '')).toThrow(
+		expect(() => new ControlOption('label', '')).toThrow(
 			'ControlOption: Value must be a non empty string.'
 		);
 	});
