acf4999d7d938e6c326f4493d8e788d99f15a078
guido <dev@guidoscialfa.com>
2024-03-10 20:11:35 +0100

    Add Push Assets

--- .github/actions/assets-artifacts/src/tasks/push-assets.ts
+++ .github/actions/assets-artifacts/src/tasks/push-assets.ts
@@ -1,9 +1,11 @@
-import {createGit} from '../create-git'
+import { createGit } from "../create-git";
 
-export async function pushAssets (): Promise<void> {
-	const git = createGit()
+export async function pushAssets(): Promise<void> {
+	const git = createGit();
 
-	return git.add('./build')
-	  .then(() => git.commit('🚀 Build Artifacts'))
-	  .then(() => git.push()).then(() => {})
+	return git
+		.add("./build")
+		.then(() => git.commit("🚀 Build Artifacts"))
+		.then(() => git.push())
+		.then(() => {});
 }
