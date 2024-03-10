7856ea7bde6cfed7bf196d25484e92f2e8c78aa4
guido <dev@guidoscialfa.com>
2024-03-10 10:20:07 +0100

    Hard coding Build Command
    --skip-assets-artifacts

--- .github/actions/assets-artifacts/src/main.ts
+++ .github/actions/assets-artifacts/src/main.ts
@@ -2,15 +2,9 @@ import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 
 async function main(): Promise<void> {
-	const command = core.getInput("build-command");
-
-	if (!command) {
-		core.setFailed("Missing command. Aborting!");
-	}
-
 	core.info("Starting Assets Compilation...");
 
-	exec.exec(command)
+	exec.exec('yarn build')
 		.then(() => core.info("Compilation done."))
 		.catch((error) => core.setFailed(`Failed to compile assets: ${error}`));
 }
