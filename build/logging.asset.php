b29be7c255df1ddde5f16f80bb55799b78cb0cd3
guido <dev@guidoscialfa.com>
2024-03-13 23:20:59 +0100

    remove build

--- build/logging.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => '2849eb862d5a52f85caa');
--- build/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-compose', 'wp-core-data', 'wp-hooks', 'wp-i18n'), 'version' => '7bb16ef3c81b1367c924');
