ea15786b0178f68c112135d6bc0898001b89fbd8
bot <bot.bot@bot.bot>
2024-04-11 16:35:53 +0000

    🚀 Build Artifacts

---
+++ build/logging.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-hooks'), 'version' => '2849eb862d5a52f85caa');
---
+++ build/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-compose', 'wp-core-data', 'wp-hooks', 'wp-i18n'), 'version' => '4f74c16c74850fe9ec4a');
