3a5a51af395ea9c271caf4a78e57821d7aa56628
Guido Scialfa <dev@guidoscialfa.com>
2024-06-26 23:46:26 +0200

    Rename Vendor
    rename references from Widoz to Spaghetti Dojo after moving the repository

--- sources/server/src/Library.php
+++ sources/server/src/Library.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Widoz\Wp\EntitiesSearch;
+namespace SpaghettiDojo\Wp\EntitiesSearch;
 
 use Inpsyde\Modularity;
 
--- sources/server/src/Modules/Logging/Module.php
+++ sources/server/src/Modules/Logging/Module.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Widoz\Wp\EntitiesSearch\Modules\Logging;
+namespace SpaghettiDojo\Wp\EntitiesSearch\Modules\Logging;
 
 use Inpsyde\Modularity;
 use Psr\Container;
